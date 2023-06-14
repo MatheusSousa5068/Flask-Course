e0f1f24e6d98c873493d69d825f47678404a281e
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-14 09:27:35 -0300

    template forms 73

--- flask_with_templates/basic.py
+++ flask_with_templates/basic.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/user/<name>')
-def assertPage(name):
-    return render_template('assert.html', name=name)
+@app.route('/signup_form')
+def signup_form():
+    return render_template('signup.html')
 
+@app.route('/thank_you')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html', first=first, last=last)
 
-
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
