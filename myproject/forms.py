3d62cc1288b21515ccaf5ec2e38a96ada557d0ff
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-19 10:33:10 -0300

    flask login part 3 -100

--- myproject/forms.py
+++ myproject/forms.py
@@ -12,11 +12,13 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_confirmation', message='Passwords do not match')])
-    password_confirmation = PasswordField('Confirm Password', validators=[EqualTo()])
+    password_confirmation = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register!')
 
-    def check_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('That email has already been registerd')
+    def validate_email(self, email):
+        if User.query.filter_by(email=self.email.data).first():
+            raise ValidationError('Email has been registered')
 
-    def check_username(self, field):
+    def validate_username(self, username):
+        if User.query.filter_by(username=self.username.data).first():
+            raise ValidationError('Username has been registered')
