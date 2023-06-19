91754d4a7248425dacd0f4d056f65980d7a54e76
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-19 11:52:41 -0300

    flask oauth with google part one - 103

---
+++ oauth/app.py
@@ -0,0 +1,44 @@
+import os
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = '1'
+
+from flask import Flask, redirect, url_for, render_template
+from flask_dance.contrib.google import make_google_blueprint, google
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+blueprint = make_google_blueprint(
+    client_id=os.environ.get('CLIENT_ID'),
+    client_secret=os.environ.get('CLIENT_SECRET'),
+    offline=True
+)
+
+app.register_blueprint(blueprint, url_prefix='/login')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/login/google')
+def login():
+    if not google.authorized:
+        return render_template(url_for('google.login'))
+
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok, resp.text
+    email = resp.json()['email']
+
+    return render_template('welcome.html', email=email)
+
+@app.route('/welcome')
+def welcome():
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok, resp.text
+    email = resp.json()['email']
+
+    return render_template('welcome.html', email=email)
