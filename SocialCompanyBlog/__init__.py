42c2edae01af8184526aeb2db74b26171fe5767d
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-20 10:03:51 -0300

    Error pages 108

--- SocialCompanyBlog/__init__.py
+++ SocialCompanyBlog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from SocialCompanyBlog.core.views import core
+from SocialCompanyBlog.error_pages.handlers import error_pages
 
 app = Flask(__name__)
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
