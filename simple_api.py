e2d9fedb4aabe307bf2dde1246a0e31b102fe225
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-22 09:25:05 -0300

    CRUD Rest basics 121

--- simple_api.py
+++ simple_api.py
@@ -2,15 +2,43 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
 api = Api(app)
 
-class HelloWorld(Resource):
+
+pets = [
+    {'name': 'Rufus'}
+]
+
+class PetNames(Resource):
+    def get(self, name):
+        for pet in pets:
+            if pet['name'] == name:
+                return pet
+
+        return {'name': None}, 404
+
+    def post(self, name):
+        pet = {'name': name}
+        pets.append(pet)
+
+        return pet
+
+    
+    def delete(self, name):
+        for i, pet in enumerate(pets):
+            if pet['name'] == name:
+                deleted = pets.pop(i)
+                return {'note': f'deleted {deleted}'}
+        return {'name': None}, 404
+
+
+class AllNames(Resource):
     def get(self):
-        return {'hello': "hello, world!"}
+        return {'pets': pets}
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(PetNames, '/pet/<string:name>')
+api.add_resource(AllNames, '/pets/')
 
 if __name__ == "__main__":
     app.run(debug=True)
