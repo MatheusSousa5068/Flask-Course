8a12214cb425ead02586814944b594be8bd13154
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-22 10:16:45 -0300

    auth w/ flask-jwt - 122

--- simple_api.py
+++ simple_api.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from secure_check import authenticate, identity
+from flask_jwt import JWT, jwt_required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
 
 
 pets = [
@@ -33,6 +38,7 @@ class PetNames(Resource):
 
 
 class AllNames(Resource):
+    @jwt_required()
     def get(self):
         return {'pets': pets}
 
